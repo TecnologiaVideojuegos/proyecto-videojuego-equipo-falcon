bee794dbf35a2fd6daa1dffc9e7b4c6f32e64a2e
LucasGviedma <lugasgarciav@edu.uah.es>
2019-04-15 23:06:13 +0200

    commit 30

--- src/Elementos/Pelota.java
+++ src/Elementos/Pelota.java
@@ -5,12 +5,12 @@
  */
 package Elementos;
 
-import Personajes.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Rectangle;
 
 /**
@@ -25,15 +25,12 @@ public class Pelota {
     Animation actual, aparece, gira;
     int[] duration2 = {500, 500,110};
     int[] duration = {100, 100, 100,100};
-    Rectangle h1,h2,h3,h4;
+    Circle h1;
     private float velocidadX, velocidadY;
     public Pelota() {
         try {
             
-            h1 = new Rectangle(coordenadaX+17, coordenadaY+32, 30, 1);
-            h2 = new Rectangle(coordenadaX+16, coordenadaY+33, 1, 30);
-            h3 = new Rectangle(coordenadaX+48, coordenadaY+33, 1, 30);
-            h4 = new Rectangle(coordenadaX+17, coordenadaY+63, 30, 1);
+            h1 = new Circle(coordenadaX+5, coordenadaY+5, 26);
             
             Image[] movementUp = {new Image("SpriteBoss1\\crec1.png"), new Image("SpriteBoss1\\crec2.png"), new Image("SpriteBoss1\\crec3.png")};
             Image[] movementDown = {new Image("SpriteBoss1\\balones3.png"), new Image("SpriteBoss1\\balones4.png"), new Image("SpriteBoss1\\balones1.png"), new Image("SpriteBoss1\\balones2.png")};
@@ -80,24 +77,18 @@ public class Pelota {
         return actual;
     }
 
-    public void setDestino(float destinoX, float destinoY)
+    public void setDestinoN(float destinoX, float destinoY)
     {
         
-        velocidadX=(coordenadaX-destinoX)/500;
-        velocidadY=(coordenadaY-destinoY)/500;
+        velocidadX=(coordenadaX-destinoX)/350;
+        velocidadY=(coordenadaY-destinoY)/350;
         
     }
+    
     public float getCoordenadaX() {
         return coordenadaX;
     }
 
-    public void setCoordenadaX(float coordenadaX) {
-        this.coordenadaX = coordenadaX;
-        h1.setBounds(coordenadaX+17, coordenadaY+32, 30, 1);
-        h2.setBounds(coordenadaX+16, coordenadaY+33, 1, 30);
-        h3.setBounds(coordenadaX+48, coordenadaY+33, 1,30);
-        h4.setBounds(coordenadaX+17, coordenadaY+64, 30, 1);
-    }
 
     public float getCoordenadaY() {
         return coordenadaY;
@@ -107,31 +98,14 @@ public class Pelota {
     {
         coordenadaX=coordenadaX-velocidadX;
         coordenadaY=coordenadaY-velocidadY;
-    }
-    public void setCoordenadaY(float coordenadaY) {
-       
-        this.coordenadaY = coordenadaY;
-        h1.setBounds(coordenadaX+17, coordenadaY+32, 30, 1);
-        h2.setBounds(coordenadaX+16, coordenadaY+33, 1, 30);
-        h3.setBounds(coordenadaX+48, coordenadaY+33, 1, 30);
-        h4.setBounds(coordenadaX+17, coordenadaY+64, 30, 1);
         
+        h1.setLocation(coordenadaX+5, coordenadaY+5);
     }
+
     
-    public Rectangle getH1() {
+    public Circle getH1() {
         return h1;
     }
 
-    public Rectangle getH2() {
-        return h2;
-    }
-
-    public Rectangle getH3() {
-        return h3;
-    }
-
-    public Rectangle getH4() {
-        return h4;
-    }
     
 }
--- src/Estados/Boss1.java
+++ src/Estados/Boss1.java
@@ -8,6 +8,7 @@ package Estados;
 import Elementos.Pelota;
 import Personajes.MainChar;
 import Personajes.ProfEd;
+import static java.lang.Math.random;
 import java.util.ArrayList;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -22,30 +23,29 @@ import org.newdawn.slick.tiled.TiledMap;
  *
  * @author lucas
  */
-public class Boss1 extends BasicGameState{
-    
-    
-    private String  mapa1 = "Mapas\\boos1D.tmx";
-    private float   puntos1 []  = new float[]{1120,716,127,716,127,683,223,683,223,589,127,589,127,556,830,556,830,462,127,462,127,429,575,429,575,333,127,333,127,300,255,300,255,204,127,204,127,171,1120,171,1120,204,300,204,300,300,864,300,864,333,618,333,618,429,1120,429,1120,462,875,462,875,556,1120,556,1120,589,267,589,267,683,1120,683};
+public class Boss1 extends BasicGameState {
+
+    private String mapa1 = "Mapas\\boos1D.tmx";
+    private float puntos1[] = new float[]{1120, 716, 127, 716, 127, 683, 223, 683, 223, 589, 127, 589, 127, 556, 830, 556, 830, 462, 127, 462, 127, 429, 575, 429, 575, 333, 127, 333, 127, 300, 255, 300, 255, 204, 127, 204, 127, 171, 1120, 171, 1120, 204, 300, 204, 300, 300, 864, 300, 864, 333, 618, 333, 618, 429, 1120, 429, 1120, 462, 875, 462, 875, 556, 1120, 556, 1120, 589, 267, 589, 267, 683, 1120, 683};
     private Polygon bordes;
-    int first=10;
-    int mapaMov=0;
+    int first = 10;
+    int mapaMov = 0;
     int a = 0, b = 0, c = 0, d = 0;
     TiledMap mapa;
     MainChar personaje;
     ProfEd p1;
     ArrayList<Pelota> pelotas = new ArrayList<>();
     Pelota ball;
-    int fase=0,z=0;
+    int fase = 0, z = 0;
+
     public Boss1() {
         bordes = new Polygon(puntos1);
-        for(int i=0;i<5;i++)
-        {
-            ball= new Pelota();
+        for (int i = 0; i < 5; i++) {
+            ball = new Pelota();
             pelotas.add(ball);
         }
     }
-    
+
     @Override
     public int getID() {
         return 1;
@@ -53,16 +53,17 @@ public class Boss1 extends BasicGameState{
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-        mapa = new TiledMap(mapa1,"\\Construccion Mapas\\");
+        mapa = new TiledMap(mapa1, "\\Construccion Mapas\\");
         personaje = new MainChar();
         p1 = new ProfEd();
         pelotas = new ArrayList<>();
         personaje.setCoordenadaX(703);
         personaje.setCoordenadaY(651);
-        for(int i=0;i<10;i++){
+        for (int i = 0; i < 10; i++) {
             ball = new Pelota();
-            pelotas.add(ball);}
-        
+            pelotas.add(ball);
+        }
+
     }
 
     @Override
@@ -71,31 +72,25 @@ public class Boss1 extends BasicGameState{
         mapa.render(0, 0, 0);
         mapa.render(0, 0, 1);
         personaje.getDir().draw((int) personaje.getCoordenadaX(), (int) personaje.getCoordenadaY());
-        p1.getDir().draw(954,270);
-        for(int i=0;i<pelotas.size();i++)
-        {
+        p1.getDir().draw(954, 270);
+        for (int i = 0; i < pelotas.size(); i++) {
             pelotas.get(i).getDir().draw(pelotas.get(i).getCoordenadaX(), pelotas.get(i).getCoordenadaY());
+            g.draw(pelotas.get(i).getH1());
         }
-        
+
         mapa.render(0, 0, 2);
         mapa.render(0, 0, 3);
         mapa.render(0, 0, 4);
-        
-        
-        
+
         g.draw(personaje.getH1());
         g.draw(personaje.getH2());
         g.draw(personaje.getH3());
         g.draw(personaje.getH4());
-        
-        for(int i=0;i<20;i++)
-        {
-          g.draw(bordes);  
+
+        for (int i = 0; i < 20; i++) {
+            g.draw(bordes);
         }
-        
-        
-        
-     
+
     }
 
     @Override
@@ -103,173 +98,125 @@ public class Boss1 extends BasicGameState{
 
         Input input = gc.getInput();
         p1.getDir().update(i);
-        
-        if(p1.getDir().getFrame()==5)
-        {
+
+        if (p1.getDir().getFrame() == 5) {
             pelotas.get(z).init();
             pelotas.get(z).getDir().update(i);
-            
+
         }
-        for(int x=0;x<pelotas.size();x++)
-        {
-            
-            if(pelotas.get(x).getDir().getFrame()==2 && pelotas.get(x).getDir().getFrameCount()==3 && p1.getDir().getFrame()==0)
-            {   
+        for (int x = 0; x < pelotas.size(); x++) {
+
+            if (pelotas.get(x).getDir().getFrame() == 2 && pelotas.get(x).getDir().getFrameCount() == 3 && p1.getDir().getFrame() == 0) {
                 z++;
-                if(z>=pelotas.size())
-                     z=0;
-                
-                pelotas.get(x).setDestino(personaje.getCoordenadaX(), personaje.getCoordenadaY());
+                if (z >= pelotas.size()) {
+                    z = 0;
+                }
+                pelotas.get(x).setDestinoN(personaje.getCoordenadaX(), personaje.getCoordenadaY());
+
                 pelotas.get(x).setDir("gira");
                 pelotas.get(x).restart();
             }
         }
-        for(int x=0;x<pelotas.size();x++)
-        {
-            if(pelotas.get(x).getDir().getFrameCount()==4)
-            {
+        for (int x = 0; x < pelotas.size(); x++) {
+            if (pelotas.get(x).getDir().getFrameCount() == 4) {
                 pelotas.get(x).move();
                 pelotas.get(x).getDir().update(i);
             }
         }
-        
-        /*if(pelotas.get(z).getDir().getFrameCount()==3 && pelotas.get(z).getDir().getFrame()==2)
-        {
-            pelotas.get(z).setDir("gira");
-            
-        }
-        else if(p1.getDir().getFrame()==5)
-        {
-            pelotas.get(z).init();
-            pelotas.get(z).getDir().update(i);
-            pelotas.get(z).setDestino(personaje.getCoordenadaX()-16,personaje.getCoordenadaY()-16);
-            fase++;
-            System.out.println(z);
-            if(fase==3)
-            {
-                fase=0;
-                z++;
-            }
-        }if(pelotas.get(z).getDir().getFrameCount()==3 && pelotas.get(z).getDir().getFrame()==2)
-        {
-            pelotas.get(z).setDir("gira");
-            
-        }
-        for(int q=0;q<pelotas.size();q++)
-        {
-            if(pelotas.get(q).getDir().getFrameCount()==4 )
-            {
-            
-             pelotas.get(q).move();
-             pelotas.get(q).getDir().update(i);
-            }  
-        }*/
-        
-        
-        if(!input.isKeyDown(Input.KEY_0)&&first>0)
-        {
-            if(first%2==0){
-                mapa = new TiledMap("Mapas\\mapa6.tmx","\\Construccion Mapas\\");
-            }
-                else{
-                mapa = new TiledMap("Mapas\\boos1D.tmx","\\Construccion Mapas\\");}
-                first--;
-            
-            
-        }else
-        {
-            
-        //prueba fuego    
-        /*if(first%100==0){
-                mapa = new TiledMap("C:\\Users\\lucas\\Documents\\NetBeansProjects\\VideoGame\\boos1V.tmx");
-            }
-        else if(first%30==0){
-                mapa = new TiledMap("C:\\Users\\lucas\\Documents\\NetBeansProjects\\VideoGame\\boos1D.tmx");}
-                first--;    */
-        if (input.isKeyDown(Input.KEY_ENTER)) {
-            gc.exit();
-        }
-        
-        if(input.isMousePressed(Input.MOUSE_LEFT_BUTTON))
-        {
-            System.out.println(input.getMouseX() + "," + input.getMouseY() + ",");
-        }
-        //System.out.println();
-        if (input.isKeyDown(Input.KEY_W)) {
-            personaje.setDir("up");
-            personaje.getDir().update(i);
-            for(int n=0;n<20;n++)
-            {
-                if (personaje.getH1().intersects(bordes)) {
-                    a = 1;
-                    break;
-                } else {
-                    a = 0;
-                } 
-            }
-            if (a == 0 || b == 1) {
-                personaje.setCoordenadaY(personaje.getCoordenadaY() - i * 0.14f);
-                b = 0;
-            }
 
-        } else if (input.isKeyDown(Input.KEY_S)) {
-            personaje.setDir("up");
-            personaje.getDir().update(i);
-            for(int n=0;n<20;n++)
-            {
-                if (personaje.getH4().intersects(bordes)) {
-                    b = 1;
-                    break;
-                } else {
-                    b = 0;
-                } 
+        if (first > 0) {
+            if (first % 2 == 0) {
+                mapa = new TiledMap("Mapas\\mapa6.tmx", "\\Construccion Mapas\\");
+            } else {
+                mapa = new TiledMap("Mapas\\boos1D.tmx", "\\Construccion Mapas\\");
             }
-            if (b == 0 || a == 1) {
-                personaje.setCoordenadaY(personaje.getCoordenadaY() + i * 0.14f);
-                a = 0;
-            }
-
-        } else if (input.isKeyDown(Input.KEY_A)) {
-            personaje.setDir("left");
-            personaje.getDir().update(i);
-            for(int n=0;n<20;n++)
-            {
-                if (personaje.getH2().intersects(bordes)) {
-                    c = 1;
-                    break;
-                } else {
-                    c = 0;
-                } 
-            }
-            if (c == 0 || d == 1) {
-                personaje.setCoordenadaX(personaje.getCoordenadaX() - i * 0.16f);
-                d = 0;
-            }
-
-        } else if (input.isKeyDown(Input.KEY_D)) {
-            personaje.setDir("right");
-            personaje.getDir().update(i);
-            for(int n=0;n<20;n++)
-            {
-                if (personaje.getH3().intersects(bordes)) {
-                    d = 1;
-                    break;
-                } else {
-                    d = 0;
-                } 
-            }
-            
-            if (d == 0 || c == 1) {
-                personaje.setCoordenadaX(personaje.getCoordenadaX() + i * 0.16f);
-                c = 0;
-
-            }
+            first--;
 
         } else {
-            personaje.setDir("stance");
-            personaje.getDir().update(i);
+            if (input.isKeyDown(Input.KEY_ENTER)) {
+                gc.exit();
+            }
+
+            for (int x = 0; x < pelotas.size(); x++) {
+                if (pelotas.get(x).getH1().intersects(personaje.getH1()) || pelotas.get(x).getH1().intersects(personaje.getH2())
+                        || pelotas.get(x).getH1().intersects(personaje.getH3()) || pelotas.get(x).getH1().intersects(personaje.getH4())) {
+
+                    personaje.setCoordenadaX(1003);
+                    personaje.setCoordenadaY(651);
+                }
+            }
+
+            if (input.isKeyDown(Input.KEY_W)) {
+                personaje.setDir("up");
+                personaje.getDir().update(i);
+                for (int n = 0; n < 20; n++) {
+                    if (personaje.getH1().intersects(bordes)) {
+                        a = 1;
+                        break;
+                    } else {
+                        a = 0;
+                    }
+                }
+                if (a == 0 || b == 1) {
+                    personaje.setCoordenadaY(personaje.getCoordenadaY() - i * 0.14f);
+                    b = 0;
+                }
+
+            } else if (input.isKeyDown(Input.KEY_S)) {
+                personaje.setDir("up");
+                personaje.getDir().update(i);
+                for (int n = 0; n < 20; n++) {
+                    if (personaje.getH4().intersects(bordes)) {
+                        b = 1;
+                        break;
+                    } else {
+                        b = 0;
+                    }
+                }
+                if (b == 0 || a == 1) {
+                    personaje.setCoordenadaY(personaje.getCoordenadaY() + i * 0.14f);
+                    a = 0;
+                }
+
+            } else if (input.isKeyDown(Input.KEY_A)) {
+                personaje.setDir("left");
+                personaje.getDir().update(i);
+                for (int n = 0; n < 20; n++) {
+                    if (personaje.getH2().intersects(bordes)) {
+                        c = 1;
+                        break;
+                    } else {
+                        c = 0;
+                    }
+                }
+                if (c == 0 || d == 1) {
+                    personaje.setCoordenadaX(personaje.getCoordenadaX() - i * 0.16f);
+                    d = 0;
+                }
+
+            } else if (input.isKeyDown(Input.KEY_D)) {
+                personaje.setDir("right");
+                personaje.getDir().update(i);
+                for (int n = 0; n < 20; n++) {
+                    if (personaje.getH3().intersects(bordes)) {
+                        d = 1;
+                        break;
+                    } else {
+                        d = 0;
+                    }
+                }
+
+                if (d == 0 || c == 1) {
+                    personaje.setCoordenadaX(personaje.getCoordenadaX() + i * 0.16f);
+                    c = 0;
+
+                }
+
+            } else {
+                personaje.setDir("stance");
+                personaje.getDir().update(i);
+            }
+
         }
-  
-    }
     }
 }
