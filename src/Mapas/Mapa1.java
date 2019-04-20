aee22e5a953da6ac732e70898f0a10d140997ae2
LucasGviedma <lugasgarciav@edu.uah.es>
2019-04-20 18:25:39 +0200

    commit 44

--- src/Mapas/Mapa1.java
+++ src/Mapas/Mapa1.java
@@ -20,30 +20,30 @@ import org.newdawn.slick.geom.Polygon;
  */
 public class Mapa1 extends MapaT {
 
-    private String mapa = "\\Mapas\\mapa1.tmx";
+    private final String mapa = "\\Mapas\\mapa1.tmx";
 
     private Polygon bordes = new Polygon();
 
-    private float borde1[] = new float[]{0, 800, 383, 800, 383, 769, 383, 737, 319, 737, 255, 737, 255, 705, 223, 705, 191, 705, 191, 673, 159, 673, 159, 641, 127, 641, 127, 609, 127, 577, 95, 577, 95, 545, 95, 513, 95, 449, 63, 449, 31, 449, 0, 449, 0, 351, 31, 351, 63, 351, 95, 351, 95, 289, 95, 225, 127, 225, 127, 193, 127, 161, 159, 161, 159, 129, 191, 129, 191, 97, 223, 97, 255, 97, 255, 65, 287, 65, 319, 65, 351, 65, 383, 65, 383, 0, 0, 0};
-    private float borde2[] = new float[]{479, 0, 479, 64, 607, 64, 607, 96, 671, 96, 671, 128, 703, 128, 703, 160, 735, 160, 735, 224, 767, 224, 767, 352, 799, 352, 799, 320, 863, 320, 863, 160, 1151, 160, 1183, 192, 1247, 192, 1247, 160, 1407, 160, 1407, 256, 1247, 256, 1247, 352, 1119, 352, 1119, 448, 1247, 448, 1247, 544, 1407, 544, 1407, 640, 1247, 640, 1247, 800, 768, 800, 768, 768, 768, 736, 800, 736, 800, 704, 832, 704, 832, 639, 864, 639, 864, 478, 800, 478, 800, 447, 768, 447, 768, 575, 736, 575, 736, 639, 704, 639, 704, 671, 672, 671, 672, 703, 608, 703, 608, 735, 480, 735, 480, 800, 1504, 800, 1504, 0};
-    private ArrayList<float[]> puntos_bordes;
-    private ArrayList<Polygon> colisiones_bordes;
+    private final float borde1[] = new float[]{0, 800, 383, 800, 383, 769, 383, 737, 319, 737, 255, 737, 255, 705, 223, 705, 191, 705, 191, 673, 159, 673, 159, 641, 127, 641, 127, 609, 127, 577, 95, 577, 95, 545, 95, 513, 95, 449, 63, 449, 31, 449, 0, 449, 0, 351, 31, 351, 63, 351, 95, 351, 95, 289, 95, 225, 127, 225, 127, 193, 127, 161, 159, 161, 159, 129, 191, 129, 191, 97, 223, 97, 255, 97, 255, 65, 287, 65, 319, 65, 351, 65, 383, 65, 383, 0, 0, 0};
+    private final float borde2[] = new float[]{479, 0, 479, 64, 607, 64, 607, 96, 671, 96, 671, 128, 703, 128, 703, 160, 735, 160, 735, 224, 767, 224, 767, 352, 799, 352, 799, 320, 863, 320, 863, 160, 1151, 160, 1183, 192, 1247, 192, 1247, 160, 1407, 160, 1407, 256, 1247, 256, 1247, 352, 1119, 352, 1119, 448, 1247, 448, 1247, 544, 1407, 544, 1407, 640, 1247, 640, 1247, 800, 768, 800, 768, 768, 768, 736, 800, 736, 800, 704, 832, 704, 832, 639, 864, 639, 864, 478, 800, 478, 800, 447, 768, 447, 768, 575, 736, 575, 736, 639, 704, 639, 704, 671, 672, 671, 672, 703, 608, 703, 608, 735, 480, 735, 480, 800, 1504, 800, 1504, 0};
+    private final ArrayList<float[]> puntos_bordes;
+    private final ArrayList<Polygon> colisiones_bordes;
 
-    private float salida1[] = new float[]{390, 0, 474, 0, 474, 2, 390, 2};
-    private float salida2[] = new float[]{390, 800, 474, 800, 474, 798, 390, 798};
-    private float salida3[] = new float[]{0, 353, 0, 445, 2, 445, 2, 353};
-    private float salida4[] = new float[]{776, 798, 1241, 798, 1241, 800, 776, 800};
-    private ArrayList<float[]> puntos_salidas;
-    private ArrayList<Polygon> colisiones_salidas;
-    private ArrayList<Polygon> salidas_modo2;
+    private final float salida1[] = new float[]{390, 0, 474, 0, 474, 2, 390, 2};
+    private final float salida2[] = new float[]{390, 800, 474, 800, 474, 798, 390, 798};
+    private final float salida3[] = new float[]{0, 353, 0, 445, 2, 445, 2, 353};
+    private final float salida4[] = new float[]{776, 798, 1241, 798, 1241, 800, 776, 800};
+    private final ArrayList<float[]> puntos_salidas;
+    private final ArrayList<Polygon> colisiones_salidas;
+    private final ArrayList<Polygon> salidas_modo2;
 
     private int modo = 0;
 
     //plaza arriba - plaza abajo - plaza izquierda - plaza abajo
-    private float mapas[] = new float[]{3, 4, 7, 4};
-    private float coordenadas_aparicion[] = new float[]{0, 720, 0, 0, 1438, 0, 0, 0};
+    private final float mapas[] = new float[]{3, 4, 7, 4};
+    private final float coordenadas_aparicion[] = new float[]{0, 720, 0, 0, 1438, 0, 0, 0};
 
-    private ArrayList<WanderTipoT> personajes = new ArrayList<>();
+    private final ArrayList<WanderTipoT> personajes = new ArrayList<>();
     WanderTipo2 viejo1 = new WanderTipo2(1160, 480);
     WanderTipo3 viejo2 = new WanderTipo3("left", 920, 150);
     WanderTipo3 viejo3 = new WanderTipo3("right", 880, 150);
--- src/Mapas/Mapa2.java
+++ src/Mapas/Mapa2.java
@@ -18,20 +18,21 @@ import org.newdawn.slick.geom.Polygon;
  */
 public class Mapa2 extends MapaT{
     
-    private String  mapa = "\\Mapas\\mapa2.tmx";
+    private final String  mapa = "\\Mapas\\mapa2.tmx";
     
     private Polygon bordes = new Polygon();
     
-    private float   borde1 []  = new float[]{830,701,712,701,712,674,665,674,665,647,618,647,618,625,607,625,607,701,392,701,392,675,547,675,547,571,394,571,394,483,540,483,540,486,606,486,606,547,620,547,618,472,639,472,639,451,697,451,697,415,392,415,392,322,471,322,471,286,392,286,392,196,449,196,449,110,606,110,606,216,517,216,517,265,522,265,522,322,616,322,616,290,695,290,695,255,616,255,616,121,766,121,766,125,831,125,831,254,744,254,744,290,840,290,840,82,895,82,895,290,959,290,959,320,967,320,967,255,930,255,930,100,1021,100,1021,160,1088,160,1088,100,1119,100,1119,200,1088,200,1088,415,968,415,968,367,958,367,958,415,744,415,744,451,763,451,763,548,797,548,797,595,929,595,929,548,961,548,961,460,1118,460,1118,701,895,701,895,737,830,737};
-    private float   borde2 []  = new float[]{1008,609,1008,536,1075,536,1075,609};
-    private ArrayList<float[]> puntos_bordes;
-    private ArrayList<Polygon> colisiones_bordes;
+    private final float   borde1 []  = new float[]{830,701,712,701,712,674,665,674,665,647,618,647,618,625,607,625,607,701,392,701,392,675,547,675,547,571,394,571,394,483,540,483,540,486,606,486,606,547,620,547,618,472,639,472,639,451,697,451,697,415,392,415,392,322,471,322,471,286,392,286,392,196,449,196,449,110,606,110,606,216,517,216,517,265,522,265,522,322,616,322,616,290,695,290,695,255,616,255,616,121,766,121,766,125,831,125,831,254,744,254,744,290,840,290,840,82,895,82,895,290,959,290,959,320,967,320,967,255,930,255,930,100,1021,100,1021,160,1088,160,1088,100,1119,100,1119,200,1088,200,1088,415,968,415,968,367,958,367,958,415,744,415,744,451,763,451,763,548,797,548,797,595,929,595,929,548,961,548,961,460,1118,460,1118,701,895,701,895,737,830,737};
+    private final float   borde2 []  = new float[]{1008,609,1008,536,1075,536,1075,609};
+    private final ArrayList<float[]> puntos_bordes;
+
+    private final ArrayList<Polygon> colisiones_bordes;
     
-    private float   salida1[] = new float[]{832,704,891,704,891,707,832,707};
-    private float   salida2[] = new float[]{842,94,892,94,892,92,842,92};
-    private ArrayList<float[]> puntos_salidas;
-    private ArrayList<Polygon> colisiones_salidas;
-    private ArrayList<Polygon> salidas_modo2;
+    private final float   salida1[] = new float[]{832,704,891,704,891,707,832,707};
+    private final float   salida2[] = new float[]{842,94,892,94,892,92,842,92};
+    private final ArrayList<float[]> puntos_salidas;
+    private final ArrayList<Polygon> colisiones_salidas;
+    private final ArrayList<Polygon> salidas_modo2;
     
     private int modo=0;
     
